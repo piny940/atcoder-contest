faa8e05cd36ee6db1b54a3503183c4e3e0ca864e
mikan <83708535+Ansai940@users.noreply.github.com>
2022-02-09 00:31:38 +0900

    Record temporary code

--- abc236/d.py
+++ abc236/d.py
@@ -1,17 +1,17 @@
 from copy import copy
+import numpy as np
 
 
 def get_best_score(rem):
-    tmp_rem = copy(rem)
-    if len(tmp_rem) == 0:
+    if len(rem) == 0:
         return 0
     best = 0
-    me = tmp_rem[0]
-    for j in range(1, len(tmp_rem)):
+    me = rem[0]
+    tmp_rem = np.delete(rem, 0)
+    for j in range(len(tmp_rem)):
         opo = tmp_rem[j]
-        del tmp_rem[0]
-        del tmp_rem[j-1]
-        score = arr[me][opo-me-1] + get_best_score(tmp_rem)
+        tmp2 = np.delete(tmp_rem, j)
+        score = arr[me][opo-me-1] + get_best_score(tmp2)
         if score > best:
             best = score
     return best
@@ -23,6 +23,6 @@ for i in range(2 * n-1):
     li = list(map(int, input().split()))
     arr.append(li)
 
-rem = list(range(2*n))
+rem = np.array(range(2*n))
 
 print(get_best_score(rem))
