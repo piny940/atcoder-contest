719cfa69f7439121dd606a2fe18e7f954bbf3737
piny940 <an.s.shoma@gmail.com>
2023-05-04 14:02:54 +0900

    a13

--- kyopro-tessoku/chap3/a12.py
+++ kyopro-tessoku/chap3/a12.py
@@ -11,6 +11,7 @@ def num(t):
   
   return result
 
+# x以上を保ったまま探索する場合はendを含む配列で考える
 while end - start > 0:
   middle = (start + end) // 2
   if num(middle) >= k:
---
+++ kyopro-tessoku/chap3/a13.py
@@ -0,0 +1,20 @@
+n, k = list(map(int, input().split()))
+arr = list(map(int, input().split()))
+
+ans = 0
+for i in range(len(arr)):
+  smaller = arr[i]
+  start = i
+  end = len(arr)
+  
+  # x以下を保ったまま探索する場合はendを含まない配列で考える
+  while end - start > 1:
+    middle = (start + end) // 2
+    if arr[middle] - smaller <= k:
+      start = middle
+    else:
+      end = middle
+  
+  ans += start - i
+
+print(ans)  
