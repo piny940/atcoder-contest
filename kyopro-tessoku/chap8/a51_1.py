2f154ac7c7234c6a8c8ca830990f435a10586025
piny940 <an.s.shoma@gmail.com>
2023-05-17 11:26:17 +0900

    a51_1

---
+++ kyopro-tessoku/chap8/a51_1.py
@@ -0,0 +1,14 @@
+q = int(input())
+queries = []
+for i in range(q):
+  query = input().split(' ')
+  queries.append(query)
+
+stack = []
+for query in queries:
+  if query[0] == '1':
+    stack.append(query[1])
+  elif query[0] == '2':
+    print(stack[-1])
+  else:
+    stack.pop()
