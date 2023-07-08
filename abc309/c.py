f202d8a851d6d76d0da2b52df096ad2a89a97105
piny940 <an.s.shoma@gmail.com>
2023-07-08 21:47:22 +0900

    Fix c

--- abc309/c.py
+++ abc309/c.py
@@ -4,35 +4,17 @@ for i in range(n):
   a, b = list(map(int, input().split()))
   meds.append((a, b))
 
-max_day = max(map(lambda m: m[0], meds))
+meds.sort(key=lambda m: m[0])
 
-# diffにおけるindexと日付の対応{day: idx}
-day_index_map = dict()
+count = sum(map(lambda m: m[1], meds))
 
-# 飲む薬の数が変化する日
-change_days = []
-for i in range(n):
-  a = meds[i][0]
-  day_index_map[a] = i
-  change_days.append(a)
-
-diffs = [0] * len(change_days)
-
-for i in range(n):
-  a, b = meds[i]
-  diffs[i] -= b
-
-change_days.sort()
-
-prev = sum(map(lambda m: m[1], meds))
-
-if prev <= k:
+if count <= k:
   print(1)
   exit()
 
-for day in change_days:
-  idx = day_index_map[day]
-  prev += diffs[idx]
-  if prev <= k:
-    print(day + 1)
+for med in meds:
+  a, b = med
+  count -= b
+  if count <= k:
+    print(a + 1)
     exit()
