ec8af4656d556303c599c376d613482727055a51
piny940 <an.s.shoma@gmail.com>
2023-07-08 22:28:18 +0900

    e#

--- abc309/e.py
+++ abc309/e.py
@@ -1,3 +1,8 @@
+import sys
+
+# 再帰呼び出しの深さの上限を 120000 に設定
+sys.setrecursionlimit(3000000)
+
 n, m = list(map(int, input().split()))
 prr = [None] + list(map(int, input().split()))
 for i in range(1, n):
@@ -14,5 +19,25 @@ for i in range(1, n):
 
 guarded = [False] * n
 
-for i in range(m):
-  x, y = inss[i]
+ins_rest = [0] * n
+count = 0
+for x, y in inss:
+  ins_rest[x] = max(ins_rest[x], y + 1)
+
+
+def update(x):
+  if ins_rest[x] > 0:
+    guarded[x] = True
+
+  for child in children[x]:
+    ins_rest[child] = max(ins_rest[child], ins_rest[x] - 1)
+    update(child)
+
+
+update(0)
+
+for i in range(n):
+  if guarded[i]:
+    count += 1
+
+print(count)
