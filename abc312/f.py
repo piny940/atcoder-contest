a7a3f496302d878c48373270494ebbdef650a903
piny940 <an.s.shoma@gmail.com>
2023-08-01 18:49:39 +0900

    tmp

---
+++ abc312/f.py
@@ -0,0 +1,11 @@
+n, m = list(map(int, input().split()))
+items = []
+notneeds = []
+needs = []
+
+for i in range(n):
+  t, x = list(map(int, input().split()))
+  items.append((t, x))
+  if 
+  notneeds.append(x)
+  
