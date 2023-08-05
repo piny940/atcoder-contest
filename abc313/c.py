5293627db9848297a03ea73399c20d9946a3db74
piny940 <an.s.shoma@gmail.com>
2023-08-05 21:56:42 +0900

    c

--- abc313/c.py
+++ abc313/c.py
@@ -10,18 +10,28 @@ for i in range(1, n):
   diffs1.append(arr[i] - arr[i - 1])
 diffs2 = [-v for v in diffs1]
 
+if len(diffs1) == 0:
+  print(0)
+  exit()
+
 idx1 = 0
-idx2 = n - 1
+idx2 = n - 2
 
 while idx1 < idx2:
-  abs1 = abs(diffs1[idx1])
-  abs2 = abs(diffs2[idx2])
+  abs1 = abs(diffs1[idx1]) * (idx1 + 1)
+  abs2 = abs(diffs2[idx2]) * (n - idx2 - 1)
+  if abs1 == 0:
+    idx1 += 1
+    continue
+  if abs2 == 0:
+    idx2 -= 1
+    continue
   if abs1 > abs2:
-    diffs1[idx1] -= abs2
+    diffs1[idx1] -= abs2 / (idx1 + 1)
     idx2 -= 1
     count += abs2
   elif abs1 < abs2:
-    diffs2[idx2] -= abs1
+    diffs2[idx2] += abs1 / (n - idx2 - 1)
     idx1 += 1
     count += abs1
   elif abs1 == abs2:
@@ -29,4 +39,5 @@ while idx1 < idx2:
     abs2 -= 1
     count += abs1
 
-count += (diffs1[idx1] - diffs2[idx2])
+count += (diffs1[idx1] * (idx1 + 1) - diffs2[idx2] * (n - idx2 - 1)) // 4
+print(int(count))
